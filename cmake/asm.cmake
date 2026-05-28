a8fec1285922330799a13eb33d5b2f6ec29adf1d
PrivacyIsARight <vexalous@gmail.com>
2026-05-28 18:34:06 -0400

    Update c++ standard to 26 (#7)

--- cmake/asm.cmake
+++ cmake/asm.cmake
@@ -1,13 +1,14 @@
-# Assembly support - RandomX uses its own JIT compiler, no separate asm library needed.
 set(RXS_ASM_SOURCES "")
 set(RXS_ASM_LIBRARY "")
 
 if (WITH_ASM AND NOT RXS_ARM AND NOT RXS_RISCV AND CMAKE_SIZEOF_VOID_P EQUAL 8)
-    add_definitions(/DRXS_FEATURE_ASM)
+    add_definitions(-DRXS_FEATURE_ASM)
     set(RXS_ASM_SOURCES
         src/crypto/common/Assembly.h
         src/crypto/common/Assembly.cpp
+        src/crypto/randomx/jit_compiler_x86_static.S
     )
+    set_source_files_properties(src/crypto/randomx/jit_compiler_x86_static.S PROPERTIES LANGUAGE ASM)
 else()
-    remove_definitions(/DRXS_FEATURE_ASM)
+    remove_definitions(-DRXS_FEATURE_ASM)
 endif()
